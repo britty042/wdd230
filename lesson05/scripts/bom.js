c98798446ed7ac4ff5853a490f990e5a2315a785
britty042 <brittney.law@gmail.com>
2024-06-03 18:48:50 -0600

    Learning Activities 7

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -5,6 +5,13 @@ let el = document.getElementById("close-button");
 
 button.addEventListener("click", () => {
     if (input.value !== "") {
+        displayList(input.value);
+        chaptersArray.push(input.value);
+
+        setChapterList();
+        input.value = "";
+        input.focus();
+
         const li = document.createElement("li");
         const deleteButton = document.createElement("button");
         
@@ -25,11 +32,46 @@ button.addEventListener("click", () => {
     
     else {
         window.alert("Please enter a Book and a Chapter");
-    }
-    ;
+    };
 });
 
 console.log(el.ariaLabel); // "Close"
 el.ariaLabel = "Close dialog";
 console.log(el.ariaLabel); // "Close dialog"
+
+const chaptersArray = getChaptersList() || [];
+
+chaptersArray.froEach(chapter => {
+    displayList(chapter);
+});
+
+function displayList(item) {
+    const li = document.createElement("li");
+    const deleteButton = document.createElement("button");
+    
+    li.innerHTML = input.value;
+    deleteButton.textContent = "❌";
+   
+    li.appendChild(deleteButton);
+    list.appendChild(li);
+
+    deleteButton.addEventListener("click", () => {
+        list.removeChild(li);
+        deleteChapter(li.innerHTML);
+        input.focus();
+    });
+}
+
+function setChapterList() {
+    localStorage.setItem("list", JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem("list"));
+}
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length -1);
+    chaptersArray = chaptersArray.filter((item) => item !== chapter);
+    setChapterList();
+}
 
