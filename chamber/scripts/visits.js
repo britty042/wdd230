f0a363a96b22000f066454dc04df0b10961a18d0
britty042 <brittney.law@gmail.com>
2024-06-05 19:14:12 -0600

    meta ridiculousness

--- chamber/scripts/visits.js
+++ chamber/scripts/visits.js
@@ -1,10 +1,32 @@
-const lastVisted = document.querySelector(".today");
-const visitCountKey = "visit-count-js";
+const msToDays = 84600000;
+const todayDate = new Date();
+const today = Date.now();
+let days = "";
 
-let visitNumber = Number(localStorage.getItem(visitCountKey)) || 0;
+const lastVisited = document.querySelector(".date");
+const lastDateKey = "last-date-js";
 
-if (visitNumber == 0) {
-    lastVisted = `Welcome!  Let us know if you have any questions.`
+let visitDate = Number(localStorage.getItem(lastDateKey)) || 0;
+
+if  (visitDate == 0) {
+    lastVisited.textContent = `Welcome!  Let us know if you have any questions.`;
 }
 
-if (visitNumber)
+else if ((today-visitDate) < msToDays) {
+    lastVisited.textContent = `Back so soon!  Awesome!`;
+}
+
+else {
+    days = (today-visitDate) / msToDays;
+    wholeDays = Math.round(days);
+
+    if (wholeDays == 1) {
+        lastVisited.textContent = `You last visted one day ago.`;
+    }
+
+    else {
+        lastVisited.textContent = `You last visted ${wholeDays} days ago.`;
+    }
+}
+
+localStorage.setItem(lastDateKey, today);
