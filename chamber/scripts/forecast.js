2edb35f4ca47a21b478c1bdf92a25c75c772a6a7
britty042 <brittney.law@gmail.com>
2024-06-26 17:49:50 -0600

    help!

---
+++ chamber/scripts/forecast.js
@@ -0,0 +1,106 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const currentTemp = document.querySelector("#current-temp");
+    const captionDesc = document.querySelector("figcaption");
+    const weatherIcon = document.querySelector("#weather-icon");
+
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=41.14&lon=-112.05&appid=e356e16f0092b81f81328a0edd061471&units=imperial`;
+
+    async function apiFetch() {  
+        try {
+            const response = await fetch(url);
+            if (response.ok)  {
+                const data = await response.json();
+                groupedForecasts = getTempsByDate(data.list)
+                displayCurrentTemp(data.list[0]);
+                displayForecastTemps(groupedForecasts);
+                console.log(data);
+            }
+            else {
+                throw Error(await response.text());
+            }}
+        catch (error) {
+            console.log(error);
+        }
+    }
+    function getTempsByDate(forecastData) {
+        const dailyForecasts = {};
+    
+        forecastData.forEach((forecast) => {
+            const forecastDate = new Date(forecast.dt * 1000).toLocaleDateString();
+    
+            if (!dailyForecasts[forecastDate]) {
+                dailyForecasts[forecastDate] = [];
+            }
+    
+            dailyForecasts[forecastDate].push(forecast);
+        });
+    
+        return Object.values(dailyForecasts);
+    }
+
+    function displayCurrentTemp(data) {
+        const temperature = Math.round(data.main.temp);
+        currentTemp.innerHTML = `${temperature}&deg;F`;
+        const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+        
+        let weatherDescriptions = data.weather.map(item => {
+            let desc = item.description;
+            const words = desc.split(" ");
+            for (let i = 0; i < words.length; i ++) {
+                words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+            }
+            return words.join(" ");
+        });
+        
+        const capitalizedDesc = weatherDescriptions.join(" ");
+        weatherIcon.setAttribute('src', iconsrc);
+        weatherIcon.setAttribute('alt', capitalizedDesc);
+        captionDesc.textContent = `${capitalizedDesc}`;
+    }
+
+    function displayForecastTemps(dailyForecasts) {
+        let forecastList = document.querySelector("#forecast-list");
+        
+
+        dailyForecasts.forEach((forecast, index) => {       
+
+            const forecastedIconsrc = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+        
+            let forecastedWeatherDescriptions = forecast.weather.map(item => {
+                let desc = item.description;
+                const words = desc.split(" ");
+                for (let i = 0; i < words.length; i ++) {
+                    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+                }
+            return words.join(" ");
+            });
+            
+            const capitalizedDesc = forecastedWeatherDescriptions.join(" ");
+
+            let card = document.createElement("section");
+            let weekday = document.createElement("h4");
+            let forecastTemp = document.createElement("h5");
+            let icon = document.createElement("figure");
+            let forecastedWeatherIcon = document.createElement("img");
+            let captionDesc = document.createElement("p");
+
+            let forecastDate = new Date(forecast.dt * 1000);
+            weekday.textContent = forecastDate.toLocaleDateString("en-US", { weekday: "long" });
+
+            forecastedWeatherIcon.setAttribute('src', forecastedIconsrc);
+            forecastedWeatherIcon.setAttribute('alt', capitalizedDesc);
+            captionDesc.textContent = `${capitalizedDesc}`;
+
+            forecastTemp.textContent = `Temperature: ${Math.round(forecast.temp.day)}°F`;
+
+            icon.appendChild(forecastedWeatherIcon);
+            card.appendChild(weekday);
+            card.appendChild(forecastTemp);
+            card.appendChild(captionDesc);
+
+            forecastList.appendChild(card);
+        });
+    };
+
+apiFetch();
+});
